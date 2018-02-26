78847b29897926cd7c85e6d64f3da7cae7ee6847
Thalia Villalobos <vill9422@m506-118-26440.local>
2018-02-26 15:50:37 -0800

    set up the home feed

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -15,8 +15,8 @@ import KeychainAccess
 class APIManager: SessionManager {
     
     // MARK: TODO: Add App Keys
-    static let consumerKey = "YOUR_KEY_HERE"
-    static let consumerSecret = "YOUR_SECRET_HERE"
+    static let consumerKey = "9OO9ZRaMOv7sU9N7MaCsUOC4y"
+    static let consumerSecret = "zAuUNflMNw9sh4gdx7OhfLHhz36Ez7VgDmjhmBx1TBkpyUxceA"
 
     static let requestTokenURL = "https://api.twitter.com/oauth/request_token"
     static let authorizeURL = "https://api.twitter.com/oauth/authorize"
