c874ea8c912a4866264613eec91f3cb1cb7ec2c9
Thalia Villalobos <vill9422@m506-246-25987.local>
2018-03-09 16:50:12 -0800

    added a profile view controller

---
+++ twitter_alamofire_demo/ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Thalia Villalobos on 3/9/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onTapHome(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
