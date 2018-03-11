b00b889d1f64d29aa81fa3e69ce5548e0c8f2618
Thalia <thalia@Marios-Mac.local>
2018-03-10 18:27:37 -0800

    user can now reply to a tweet

--- twitter_alamofire_demo/DetailViewController.swift
+++ twitter_alamofire_demo/DetailViewController.swift
@@ -119,16 +119,13 @@ class DetailViewController: UIViewController {
         
     }
     
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "replySegue" {
+            print("About to reply")
+            let replyViewController = segue.destination as! ReplyViewController
+            replyViewController.tweet = tweet
+        }
     }
-    */
+ 
 
 }
